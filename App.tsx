85ed439c65f8ca005583aff7055b4d06315e7d8e
Andrei Yeutukhou <gavnorock84@hotmail.com>
2024-05-28 23:15:10 +0300

    add radio

--- App.tsx
+++ App.tsx
@@ -1,14 +1,33 @@
-import {FC} from "react";
-import {StyleSheet, Text, View} from 'react-native';
-import {StatusBar} from 'expo-status-bar';
+import { FC, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { RadioButton } from 'react-native-paper'
 
 const App: FC = () => {
+  const [checked, setChecked] = useState<string>('first')
+
   return (
     <View style={styles.container}>
       <Text>Bhagavad Gita Quiz App</Text>
-      <StatusBar style="auto"/>
+      <RadioButton
+        color="blue"
+        value="first"
+        status={checked === 'first' ? 'checked' : 'unchecked'}
+        onPress={() => setChecked('first')}
+      />
+      <RadioButton
+        value="second"
+        status={checked === 'second' ? 'checked' : 'unchecked'}
+        onPress={() => setChecked('second')}
+      />
+      <RadioButton
+        value="second"
+        status={checked === 'third' ? 'checked' : 'unchecked'}
+        onPress={() => setChecked('third')}
+      />
+      <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -16,8 +35,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#2d2c2c',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
 
 export default App
