6fa577792d3c5621dc23567d9986d2b17d152376
Andrei Yeutukhou <gavnorock84@hotmail.com>
2024-05-30 22:54:41 +0300

    add some options and styles

--- App.tsx
+++ App.tsx
@@ -1,31 +1,52 @@
-import { FC, useState } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { StatusBar } from 'expo-status-bar'
-import { RadioButton } from 'react-native-paper'
+import {FC, useState} from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+import {StatusBar} from 'expo-status-bar'
+import {RadioButton} from 'react-native-paper'
 
 const App: FC = () => {
   const [checked, setChecked] = useState<string>('first')
+  const primary: string = '#006fee'
 
   return (
     <View style={styles.container}>
-      <Text>Bhagavad Gita Quiz App</Text>
-      <RadioButton
-        color="blue"
-        value="first"
-        status={checked === 'first' ? 'checked' : 'unchecked'}
-        onPress={() => setChecked('first')}
-      />
-      <RadioButton
-        value="second"
-        status={checked === 'second' ? 'checked' : 'unchecked'}
-        onPress={() => setChecked('second')}
-      />
-      <RadioButton
-        value="second"
-        status={checked === 'third' ? 'checked' : 'unchecked'}
-        onPress={() => setChecked('third')}
-      />
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bhagavad Gita Quiz App</Text>
+
+      <View>
+        <Text style={styles.question}>
+          What is the name of Lord Krishna’s seashell?
+        </Text>
+      </View>
+
+      <Text style={styles.task}>Choose the correct answer:</Text>
+
+      <View style={styles.option}>
+        <RadioButton
+          color={primary}
+          value="first"
+          status={checked === 'first' ? 'checked' : 'unchecked'}
+          onPress={() => setChecked('first')}
+        />
+        <Text>Option 1</Text>
+      </View>
+      <View style={styles.option}>
+        <RadioButton
+          color={primary}
+          value="second"
+          status={checked === 'second' ? 'checked' : 'unchecked'}
+          onPress={() => setChecked('second')}
+        />
+        <Text>Option 2</Text>
+      </View>
+      <View style={styles.option}>
+        <RadioButton
+          color={primary}
+          value="third"
+          status={checked === 'third' ? 'checked' : 'unchecked'}
+          onPress={() => setChecked('third')}
+        />
+        <Text>Option 3</Text>
+      </View>
+      <StatusBar style="auto"/>
     </View>
   )
 }
@@ -33,9 +54,32 @@ const App: FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#2d2c2c',
+    padding: 8,
+    backgroundColor: '#a19e9e',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  title: {
+    fontSize: 26,
+    textDecorationLine: 'underline',
+  },
+  option: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    padding: 5,
+    marginBottom: 5,
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 7,
+  },
+  task: {
+    fontSize: 18,
+  },
+  question: {
+    textAlign: 'left',
+    fontSize: 20,
   }
 })
 
