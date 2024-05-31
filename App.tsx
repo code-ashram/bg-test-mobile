65b218fb0fb032697b53832cb59fca57920685a4
Andrei Yeutukhou <gavnorock84@hotmail.com>
2024-05-31 20:46:04 +0300

    emprove styles for project

--- App.tsx
+++ App.tsx
@@ -1,15 +1,26 @@
-import {FC, useState} from 'react'
-import {StyleSheet, Text, View} from 'react-native'
-import {StatusBar} from 'expo-status-bar'
-import {RadioButton} from 'react-native-paper'
+import { FC, useState } from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import { Button, Divider, ProgressBar, RadioButton } from 'react-native-paper'
+
+const primary: string = '#006fee'
+const secondary: string = '#575555'
 
 const App: FC = () => {
   const [checked, setChecked] = useState<string>('first')
-  const primary: string = '#006fee'
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Bhagavad Gita Quiz App</Text>
+
+      <View style={styles.progress}>
+        <Text style={styles.questionNumber}>
+          Question №1
+        </Text>
+        <ProgressBar style={styles.progressBar} progress={0.1} color={primary} />
+      </View>
+
+      <Divider />
 
       <View>
         <Text style={styles.question}>
@@ -17,36 +28,44 @@ const App: FC = () => {
         </Text>
       </View>
 
-      <Text style={styles.task}>Choose the correct answer:</Text>
 
-      <View style={styles.option}>
-        <RadioButton
-          color={primary}
-          value="first"
-          status={checked === 'first' ? 'checked' : 'unchecked'}
-          onPress={() => setChecked('first')}
-        />
-        <Text>Option 1</Text>
+      <View style={styles.optionsList}>
+        <Text style={styles.task}>Choose the correct answer:</Text>
+
+        <View style={styles.option}>
+          <RadioButton
+            color={primary}
+            value="first"
+            status={checked === 'first' ? 'checked' : 'unchecked'}
+            onPress={() => setChecked('first')}
+          />
+          <Text style={styles.optionText}>Option 1</Text>
+        </View>
+        <View style={styles.option}>
+          <RadioButton
+            color={primary}
+            value="second"
+            status={checked === 'second' ? 'checked' : 'unchecked'}
+            onPress={() => setChecked('second')}
+          />
+          <Text style={styles.optionText}>Option 2</Text>
+        </View>
+        <View style={styles.option}>
+          <RadioButton
+            color={primary}
+            value="third"
+            status={checked === 'third' ? 'checked' : 'unchecked'}
+            onPress={() => setChecked('third')}
+          />
+          <Text style={styles.optionText}>Option 3</Text>
+        </View>
       </View>
-      <View style={styles.option}>
-        <RadioButton
-          color={primary}
-          value="second"
-          status={checked === 'second' ? 'checked' : 'unchecked'}
-          onPress={() => setChecked('second')}
-        />
-        <Text>Option 2</Text>
-      </View>
-      <View style={styles.option}>
-        <RadioButton
-          color={primary}
-          value="third"
-          status={checked === 'third' ? 'checked' : 'unchecked'}
-          onPress={() => setChecked('third')}
-        />
-        <Text>Option 3</Text>
-      </View>
-      <StatusBar style="auto"/>
+
+      <Button style={styles.button} mode="contained" buttonColor={primary} onPress={() => console.log('Pressed')}>
+        <Text style={styles.buttonText}>Next</Text>
+      </Button>
+
+      <StatusBar style="auto" />
     </View>
   )
 }
@@ -54,32 +73,70 @@ const App: FC = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 8,
-    backgroundColor: '#a19e9e',
-    alignItems: 'center',
-    justifyContent: 'center'
+    paddingTop: 40,
+    paddingHorizontal: 10,
+    backgroundColor: '#18181b',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start'
   },
   title: {
+    color: 'white',
+    alignSelf: 'center',
     fontSize: 26,
-    textDecorationLine: 'underline',
+    fontWeight: 'bold',
+    textDecorationLine: 'underline'
+  },
+  optionsList: {
+    marginTop: 20,
+    width: '100%'
   },
   option: {
     flexDirection: 'row',
     alignItems: 'center',
-    width: '100%',
     padding: 5,
     marginBottom: 5,
     borderStyle: 'solid',
-    borderColor: 'black',
+    borderColor: secondary,
     borderWidth: 1,
-    borderRadius: 7,
+    borderRadius: 7
+  },
+  optionText: {
+    color: 'white'
   },
   task: {
+    marginVertical: 5,
     fontSize: 18,
+    color: 'white'
   },
   question: {
-    textAlign: 'left',
+    marginTop: 20,
     fontSize: 20,
+    textAlign: 'left',
+    color: 'white'
+  },
+  questionNumber: {
+    marginTop: 8,
+    color: 'white',
+    fontSize: 18
+  },
+  progress: {
+    marginTop: 7,
+    width: '100%'
+  },
+  progressBar: {
+    marginTop: 5,
+    height: 10,
+    borderRadius: 7,
+    backgroundColor: secondary
+  },
+  button: {
+    alignSelf: 'flex-end',
+    marginTop: 5,
+    borderRadius: 9
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 18
   }
 })
 
